8570fd8fab8ef5a38f21e2c59874ef31e5c0cf5e
UDAYASRIBASAWOJU <141737684+UDAYASRIBASAWOJU@users.noreply.github.com>
2025-04-27 13:29:07 +0530

    arithmetic operations on array

---
+++ arithmetic.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+arr = np.array([3,4,5,6,8])
+
+sum = print(arr+2)
+print('')
+
+sub = print(arr-1)
+print('')
+
+mul = print(arr*3)
+print('')
+
+divp = print(arr/4) #with decimal points
+print('')
+
+mod = print(arr%2)
+print('')
+
+squ = print(arr**3)
+print('')
+
+div = print(arr//4)
+print('')
