45f08fe9ad9bd365ffb03ee250b7fedbc05a3f2f
UDAYASRIBASAWOJU <141737684+UDAYASRIBASAWOJU@users.noreply.github.com>
2025-04-27 13:37:51 +0530

    aggregate functions in numpy

---
+++ aggregate.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([3,4,5,6,8])
+
+print("sum =",np.sum(arr))
+print("Mean =",np.mean(arr))
+print("Minimum =",np.min(arr))
+print("Maximum =",np.max(arr))
+print("Standard deviation =",np.std(arr))
+print("Variance =",np.var(arr))
