ed5dddd6034bf0597e99f48caf2d9f81e2dadbb3
UDAYASRIBASAWOJU <141737684+UDAYASRIBASAWOJU@users.noreply.github.com>
2025-04-27 13:20:35 +0530

    converting datatypes of array

--- convert.py
+++ convert.py
@@ -2,11 +2,14 @@ import numpy as np
 
 array = np.array([[3,5.3,6], [2,4,7]])
 
+print(array)
 print(array.dtype)
+print('')
 
 float = array.astype(float)
 print(float)
 print(float.dtype)
+print('')
 
 int = array.astype(int)
 print(int)
