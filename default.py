f75fce7564fd1e5f24e35e0a0c6fd58c85c87cbc
UDAYASRIBASAWOJU <141737684+UDAYASRIBASAWOJU@users.noreply.github.com>
2025-04-27 12:24:33 +0530

    Creating arrays with default values

---
+++ default.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+zero_arr = np.zeros(3)
+print(zero_arr)
+print(' ')
+
+ones_arr = np.ones((2,4))
+print(ones_arr)
+print(' ')
+
+any_same_arr = np.full((3,4),6)
+print(any_same_arr)
